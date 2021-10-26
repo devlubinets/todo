79ee6e00e097105ed49d3789772ad86675a28811
mrlubynets <mr.lubinets@gmail.com>
2021-10-26 08:38:06 +0300

    Обновил

--- Structural/Flyweight/class/ClientCode.php
+++ Structural/Flyweight/class/ClientCode.php
@@ -4,13 +4,6 @@ class  ClientCode
 {
     public static function businessCode():void
     {
-       # $wareHouseFlyweight = new FlyweightFactory(['iPhon','$1199'], ['LG','$299'], ['Xiaomi','$499']);
-//         $wareHouseFlyweight = new FlyweightFactory( ["Chevrolet", "Camaro2018", "pink"],
-//             ["Mercedes Benz", "C300", "black"],
-//             ["Mercedes Benz", "C500", "red"],
-//             ["BMW", "M5", "red"],
-//             ["BMW", "X6", "white"]);
-
         $wareHouseFlyweight = new FlyweightFactory();
         $wareHouseFlyweight->getFlyweight(['iPhon','$1199']);
         $wareHouseFlyweight->getFlyweight(['LG','$299']);
