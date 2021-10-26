79c51a4063fd16ee04598c45e8e7d1b98a028209
mrlubynets <mr.lubinets@gmail.com>
2021-10-26 08:04:20 +0300

    Выделил автозагрузчик в отдельный файл PHP

---
+++ Structural/Proxy/class/Autoload.php
@@ -0,0 +1,25 @@
+<?php
+
+spl_autoload_register(function($class) {
+
+    $fn1 = 'class/' . $class .'.php';
+    $fn2 = 'interface/' . $class . '.php';
+
+    if(file_exists($fn1))
+    {
+        require $fn1;
+        echo '<b>Autoload class: ' . $class . '</b><br>';
+    }
+    else
+        echo '<b>Not autoload class: ' . $class . '.php'. '</b><br>';
+
+
+    if(file_exists($fn2))
+    {
+        require $fn2;
+        echo '<b>Autoload interface: ' . $class . '</b><br>';
+    }
+    else
+        echo '<b>Not autoload interface: ' . $class . '.php'. '</b><br>';
+
+});
