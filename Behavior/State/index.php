b342c220eb8fd6ea3e5a12f0c181d020df491df9
mrlubynets <mr.lubinets@gmail.com>
2021-11-04 10:23:27 +0200

    Паттерн состояние с UML диаграммой

--- Behavior/State/index.php
+++ Behavior/State/index.php
@@ -21,7 +21,7 @@ ClientCode::businessCode();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Visitor</title>
+    <title>State</title>
 </head>
 <body>
 
