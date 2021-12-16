2874c1fb67be455fea40b129b81385eff8a61d9a
Lubynets Kirill <mr.lubinets@gmail.com>
2021-12-16 23:52:09 +0200

    Добавил контроллер который выводит информацию на главную страницу по публичным таскам

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function showPublicItems()
+    {
+       return  view('welcome',['items' => Item::where('visibility','=',1)->get()]);
+    }
+}
