36336349890193f0eb66ebf07e0f426e9b9fcf05
Lubynets Kirill <mr.lubinets@gmail.com>
2021-12-16 23:50:20 +0200

    Изменил имя колонки с name на desc

--- database/migrations/2021_12_16_130744_create_items_table.php
+++ database/migrations/2021_12_16_130744_create_items_table.php
@@ -15,7 +15,7 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('desc');
             $table->integer('executor');
             $table->integer('visibility');
             $table->integer('leader');
