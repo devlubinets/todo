c5e6f14acf4ca5e3a3f7b29987317394fe78e21c
Lubynets Kirill <mr.lubinets@gmail.com>
2021-12-21 19:00:14 +0200

    Работаю с тестами

--- phpUnit/src/NumeralConverter.php
+++ phpUnit/src/NumeralConverter.php
@@ -8,38 +8,45 @@ class NumeralConverter
     {
         $result = '';
 
-        switch ($value) {
-            case 1:
-                $result = 'I';
-                break;
-            case 2:
-                $result = 'II';
-                break;
-            case 3:
-                $result = 'III';
-                break;
-            case 4:
-                $result = 'IV';
-                break;
-            case 5:
-                $result = 'V';
-                break;
-            case 6:
-                $result = 'VI';
-                break;
-            case 7:
-                $result = 'VII';
-                break;
-            case 8:
-                $result = 'VIII';
-                break;
-            case 9:
-                $result = 'IX';
-                break;
-            case 10:
-                $result = 'X';
-                break;
-        }
+
+
+            switch ($value) {
+                case 1:
+                    $result = 'I';
+                    break;
+                case 2:
+                    $result = 'II';
+                    break;
+                case 3:
+                    $result = 'III';
+                    break;
+                case 4:
+                    $result = 'IV';
+                    break;
+                case 5:
+                    $result = 'V';
+                    break;
+                case 6:
+                    $result = 'VI';
+                    break;
+                case 7:
+                    $result = 'VII';
+                    break;
+                case 8:
+                    $result = 'VIII';
+                    break;
+                case 9:
+                    $result = 'IX';
+                    break;
+                case 10:
+                    $result = 'X';
+                    break;
+
+            }
+
+            if(!(is_int($value) && $value > 0)) {
+        throw new RuntimeException('This method converts positive integers only');
+             }
 
         return $result;
     }
